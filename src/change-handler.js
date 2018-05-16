e89ea1ec82e56ce3cca6fa3a31d4d61895715f16
Adam <adamdhire@gmail.com>
2018-05-16 15:44:05 -0400

    k

---
+++ src/change-handler.js
@@ -0,0 +1,35 @@
+/**
+ * This class handles change for a vending machine.
+ * 
+ * IMPORTANT: All amounts are in cents. E.g. $1.35 = 135. This will help with rounding errors.
+ */
+class ChangeHandler {
+    
+    constructor(amountDue) {
+        this.amountDue = amountDue;
+        this.cashTendered = 0;
+    }
+
+    /**
+     * The customer inserts a coin, increasing the cashTendered.
+     * @param {string} type either quarter, dime, nickel, or penny
+     */
+    insertCoin(type) {
+    }
+
+    /**
+     * Returns true if enough coins have been inserted to at least meet the amountDue
+     */
+    isPaymentSufficient() {
+    }
+
+    giveChange() {
+        // TODO return the correct change in the following format...
+        return {
+            quarters: 0,
+            dimes: 0,
+            nickels: 0,
+            pennies: 0
+        }
+    }
+}
---
+++ test/change-handler.spec.js
@@ -0,0 +1,70 @@
+describe("ChangeHandler", function() {
+
+    // Constructor tests
+    it("initializes with the given amountDue", function() {
+        
+    });
+    it("initializes cashTendered to 0", function() {
+        
+    });
+
+    // Insert coin tests
+    it("insert quarter adds 25", function() {
+        
+    });
+
+    it("insert dime adds 10", function() {
+        
+    });
+
+    it("insert nickel adds 5", function() {
+        
+    });
+
+    it("insert penny adds 1", function() {
+        
+    });
+
+    it("subsequent coins continue to add value", function() {
+        
+    });
+
+    // isPaymentSufficient tests
+    it("isPaymentSufficient returns true when enough cashTendered", function() {
+        
+    });
+
+    it("isPaymentSufficient returns false when not enough cashTendered", function() {
+        
+    });
+    
+    it("isPaymentSufficient returns true when exact cashTendered", function() {
+        
+    });
+
+    // Give change tests
+    it("giveChange returns no coins when exact change", function() {
+        
+    });
+
+    it("giveChange returns correct coins for 10", function() {
+        
+    });
+
+    it("giveChange returns correct coins for 27", function() {
+        
+    });
+
+    it("giveChange returns correct coins for 68", function() {
+        
+    });
+
+    it("giveChange returns correct coins for 15", function() {
+        
+    });
+
+    it("giveChange returns correct coins for 2", function() {
+        
+    });
+
+});
